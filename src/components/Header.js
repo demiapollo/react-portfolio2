254059895ecc4db0aaad5a953e39f572abd715ea
demiapollo <apollodemirel67@gmail.com>
2023-06-30 19:11:15 -0700

    navbar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,83 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawer: {
+    width: 200,
+  },
+}));
+
 
 function Header() {
+  const classes = useStyles();
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   return (
-    <header>
-      {/* my name & navbar */}
-    </header>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Apollo Demirel
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer}>
+        <div
+          className={classes.drawer}
+          role="presentation"
+          onClick={toggleDrawer}
+          onKeyDown={toggleDrawer}
+        >
+          <List>
+            <ListItem button>
+              <ListItemText primary="About Me" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Portfolio" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Resume" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="Contact" />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+    </div>
   );
 }
 
