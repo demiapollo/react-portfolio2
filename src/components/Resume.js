463e081ae86b44ccdf2174c6be2b0264b99c9d5a
demiapollo <apollodemirel67@gmail.com>
2023-07-07 20:02:56 -0700

    pdf displays now

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { PDFViewer } from '@react-pdf/renderer';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
-import path from '../assets/Resume_DEMIREL_TECH.pdf'
+import path from "../assets/Resume_DEMIREL_TECH.pdf";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
     margin: theme.spacing(2),
+    height: "100vh",
   },
-  pdfViewer: {
-    width: '100%',
-    height: 'calc(100vh - 200px)', // Adjust the height as needed
-    marginBottom: theme.spacing(2),
-  },
+  //   pdfViewer: {
+  //     width: "100%",
+  //     height: "calc(100vh - 500px)", // Adjust the height as needed
+  //     marginBottom: theme.spacing(2),
+  //   },
 }));
 
 const Resume = () => {
@@ -25,14 +25,13 @@ const Resume = () => {
   const handleDownload = () => {
     // Logic for downloading the PDF file
     // Replace 'path/to/pdf/file.pdf' with your actual PDF file path
-    window.open('src/assets/Resume_DEMIREL_TECH.pdf', '_blank');
+    window.open("../assets/Resume_DEMIREL_TECH.pdf", "_blank");
   };
 
   return (
     <div className={classes.root}>
-      <PDFViewer className={classes.pdfViewer}>
-        <iframe src={path} width="100%" height="100%" title='My Resume' />
-      </PDFViewer>
+      <iframe src={path} width="100%" height="100%" title="Resume DEMIREL" />
+
       <Button variant="contained" color="primary" onClick={handleDownload}>
         Download
       </Button>
