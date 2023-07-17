a180cb06ebfa3354357e48b466fa50165bb4db5a
demiapollo <apollodemirel67@gmail.com>
2023-07-17 14:10:54 -0700

    contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,127 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Snackbar } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Contact() {
+const useStyles = makeStyles((theme) => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: 300,
+    margin: '0 auto',
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(2),
+    border: '1px solid #ccc',
+    borderRadius: theme.spacing(1),
+  },
+  submitButton: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const Contact = () => {
+  const classes = useStyles();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [errors, setErrors] = useState({
+    name: false,
+    email: false,
+    message: false,
+  });
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { name, email, message } = formData;
+    setErrors({
+      name: name === '',
+      email: !validateEmail(email),
+      message: message === '',
+    });
+    setOpenSnackbar(true);
+  };
+
+  const validateEmail = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSnackbar(false);
+  };
+
   return (
-    <section id="contact">
-      {/* I will add my contact details here a form*/}
-    </section>
+    <>
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <TextField
+          label="Name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          error={errors.name}
+          helperText={errors.name && 'Name is required'}
+          required
+        />
+        <TextField
+          label="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          error={errors.email}
+          helperText={errors.email && 'Invalid email address'}
+          required
+        />
+        <TextField
+          label="Message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          multiline
+          rows={5}
+          error={errors.message}
+          helperText={errors.message && 'Please leave a message.'}
+        />
+        <Button
+          className={classes.submitButton}
+          variant="contained"
+          color="primary"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+      >
+        <MuiAlert
+          onClose={handleCloseSnackbar}
+          severity="error"
+          elevation={6}
+          variant="filled"
+        >
+          {errors.name ? 'Name is required.' : ''}
+          {errors.email ? 'Invalid email address.' : ''}
+          {errors.message ? 'Please leave a message.' : ''}
+        </MuiAlert>
+      </Snackbar>
+    </>
   );
-}
+};
 
 export default Contact;
